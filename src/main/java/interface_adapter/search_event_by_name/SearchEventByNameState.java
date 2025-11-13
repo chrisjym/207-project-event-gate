3b2c4cb6ddfa4c89e2f25e804f8641d18c604297
SuperUltraCoolGuy <chris.mong@mail.utoronto.ca>
2025-11-13 13:22:08 -0500

    [chore] added Event to SearchEventByNameState

--- src/main/java/interface_adapter/search_event_by_name/SearchEventByNameState.java
+++ src/main/java/interface_adapter/search_event_by_name/SearchEventByNameState.java
@@ -1,4 +1,15 @@
 package interface_adapter.search_event_by_name;
 
+import entity.Event;
+
 public class SearchEventByNameState {
+    private Event event = new Event();
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
